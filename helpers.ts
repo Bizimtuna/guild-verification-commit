f5c87b15f689162c99a71a0146721300af3a9ee9
Bizimtuna <95029865+Bizimtuna@users.noreply.github.com>
2026-03-05 21:48:12 +0300

    Resolve null pointer exception in user service

--- helpers.ts
+++ helpers.ts
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-05T18:47:21.570Z
-Commit: Add export functionality for reports
+Timestamp: 2026-03-05T18:48:13.964Z
+Commit: Resolve null pointer exception in user service
