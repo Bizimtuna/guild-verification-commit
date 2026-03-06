1843003cacec57f1a96cebb80e227239638b0965
Bizimtuna <95029865+Bizimtuna@users.noreply.github.com>
2026-03-06 20:04:23 +0300

    Fix memory leak in event handlers

--- utils.js
+++ utils.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-06T17:04:19.724Z
-Commit: Fix broken links in documentation
+Timestamp: 2026-03-06T17:04:23.821Z
+Commit: Fix memory leak in event handlers
