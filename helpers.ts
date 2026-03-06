de951b59a7567285a1953952d0eef684cf26c74d
Bizimtuna <95029865+Bizimtuna@users.noreply.github.com>
2026-03-06 20:04:34 +0300

    Fix memory leak in event handlers

--- helpers.ts
+++ helpers.ts
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-05T19:10:14.906Z
-Commit: Patch API endpoint error handling
+Timestamp: 2026-03-06T17:04:35.316Z
+Commit: Fix memory leak in event handlers
