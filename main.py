60a8fb4e82d45f7dd9cdfbb06d2cd92332014dc8
Bizimtuna <95029865+Bizimtuna@users.noreply.github.com>
2026-03-06 20:04:22 +0300

    Fix race condition in async operations

--- main.py
+++ main.py
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-05T19:10:02.300Z
-Commit: Introduce real-time notifications
+Timestamp: 2026-03-06T17:04:22.336Z
+Commit: Fix race condition in async operations
