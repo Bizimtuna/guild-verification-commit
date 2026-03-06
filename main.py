b89764178123ec36e2ac55dbc46d476b3cc6d243
Bizimtuna <95029865+Bizimtuna@users.noreply.github.com>
2026-03-06 20:04:39 +0300

    Fix CSS styling bug on mobile devices

--- main.py
+++ main.py
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-06T17:04:33.931Z
-Commit: Resolve timezone issues in date handling
+Timestamp: 2026-03-06T17:04:40.193Z
+Commit: Fix CSS styling bug on mobile devices
