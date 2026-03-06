0acb9a1539f01b748d585967bf53fe964cc860bb
Bizimtuna <95029865+Bizimtuna@users.noreply.github.com>
2026-03-06 20:04:19 +0300

    Fix broken links in documentation

--- utils.js
+++ utils.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-06T17:04:16.915Z
-Commit: Implement user profile customization
+Timestamp: 2026-03-06T17:04:19.724Z
+Commit: Fix broken links in documentation
